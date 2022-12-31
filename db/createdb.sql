dbaac56a21e57ce1859b45ec3f2c04d345474d06
Jouni Alanen <jouni.alanen@terveystalo.com>
2022-12-31 21:49:13 +0200

    Attempt

--- db/createdb.sql
+++ db/createdb.sql
@@ -1,5 +1,3 @@
 CREATE USER nester WITH PASSWORD 'nester';
 CREATE DATABASE nesting;
-GRANT ALL PRIVILEGES ON DATABASE nesting TO nester;
-\connect nesting
-ALTER SCHEMA public OWNER TO nester;
+ALTER DATABASE nesting OWNER TO nester;
